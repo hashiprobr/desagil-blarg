a4326188844045a6e3ebd05492ea2b91d8998253
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-03-14 22:44:45 -0300

    Add initial files

---
+++ src/br/edu/insper/desagil/blarg/Post.java
@@ -0,0 +1,47 @@
+package br.edu.insper.desagil.blarg;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	private Date date;
+	private String title;
+	private String content;
+	private List<String> tags;
+
+	public Post(String title, String content) {
+		this.date = new Date();
+		this.title = title;
+		this.content = content;
+		this.tags = new ArrayList<>();
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public boolean contains(String tag) {
+		return this.tags.contains(tag);
+	}
+
+	public void addTag(String tag) {
+		this.tags.add(tag);
+	}
+}
